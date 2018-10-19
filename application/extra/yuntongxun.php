bec59ab21c8084e825184f3bd2aa179d74cebd5e
lining <1097968473@qq.com>
2018-10-19 11:20:03 +0800

    修改云通信类库

--- application/extra/yuntongxun.php
+++ application/extra/yuntongxun.php
@@ -7,12 +7,12 @@
  */
 
 return [
-    'accountSid' => "8a48b5514da42dc3014da43171a60013",
-    'accountToken' => "6f11fc603e124859b924898e104276c9",
-    'appId' => "8aaf07086436008b01643b158251042d",
-    'serverIP' => "app.cloopen.com",
-    'serverPort' => "8883",
-    'softVersion' => "2013-12-26",
-    'tempId' => "258059",
+    'accountSid' => "xxxxxxxxxxxx",
+    'accountToken' => "xxxxxxxxxxxx",
+    'appId' => "xxxxxxxxxxxx",
+    'serverIP' => "xxxxxxxxxxxx",
+    'serverPort' => "xxxxxxxxxxxx",
+    'softVersion' => "xxxx-xx-xx",
+    'tempId' => "xxxxx",
     'identify_time' => 3600 * 24
 ];
